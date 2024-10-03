2cd5d55d2e0165f0eaf8c07076e21d99a0987e86
Gustavo Guedes <gguedes.tech@gmail.com>
2024-10-03 09:33:47 -0300

    Change Spky approach to validate called urls

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -12,7 +12,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 	func test_init_doesNotRequestDataFromURL() {
 		let (_, client) = makeSUT()
 		
-		XCTAssertNil(client.requestedURL)
+		XCTAssertTrue(client.requestedURLs.isEmpty)
 	}
 	
 	func test_load_requestDataFromURL() {
@@ -21,8 +21,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
 		
 		sut.load()
 		
-		XCTAssertEqual(client.requestedURL, url)
-		XCTAssertEqual(client.requestedURLs.count, 1)
+		XCTAssertEqual(client.requestedURLs, [url])
+	}
+	
+	func test_load_requestDataFromURLTwice() {
+		let url = URL(string: "https://a-given-url.com")!
+		let (sut, client) = makeSUT(url: url)
+		
+		sut.load()
+		sut.load()
+		
+		XCTAssertEqual(client.requestedURLs, [url, url])
 	}
 	
 	//MARK: - Helpers
@@ -33,11 +42,9 @@ final class RemoteFeedLoaderTests: XCTestCase {
 	}
 	
 	private class HTTPClientSpy: HTTPClient {
-		var requestedURL: URL?
 		var requestedURLs = [URL]()
 		
 		func get(from url: URL) {
-			requestedURL = url
 			requestedURLs.append(url)
 		}
 	}
