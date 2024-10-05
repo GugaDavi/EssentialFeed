189bd96f4d988bdf3205e5f3b1fe4545c38a5911
Gustavo Guedes <gguedes.tech@gmail.com>
2024-10-05 10:19:49 -0300

    Delivers empty items array on 200 HTTP response with JSON empty list

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -64,6 +64,19 @@ final class RemoteFeedLoaderTests: XCTestCase {
 		})
 	}
 	
+	func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+		let (sut, client) = makeSUT()
+		
+		var capturedResults = [RemoteFeedLoader.Result]()
+		sut.load { capturedResults.append($0) }
+		
+		let emtpyListJson = Data("{\"items\": []}".utf8)
+		
+		client.complete(withStatusCode: 200, data: emtpyListJson)
+		
+		XCTAssertEqual(capturedResults, [.success([])])
+	}
+	
 	//MARK: - Helpers
 	
 	private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -78,12 +91,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
 		file: StaticString = #filePath,
 		line: UInt = #line
 	) {
-		var capturedErrors = [RemoteFeedLoader.Error]()
-		sut.load { capturedErrors.append($0) }
+		var capturedResults = [RemoteFeedLoader.Result]()
+		sut.load { capturedResults.append($0) }
 		
 		action()
 		
-		XCTAssertEqual(capturedErrors, [error], file: file, line: line)
+		XCTAssertEqual(capturedResults, [.failure(error)], file: file, line: line)
 	}
 	
 	private class HTTPClientSpy: HTTPClient {
