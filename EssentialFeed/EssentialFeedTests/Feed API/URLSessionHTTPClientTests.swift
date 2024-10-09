cd6e24b46256f2a27f18f76756560cb8ef4c90cf
Gustavo Guedes <gguedes.tech@gmail.com>
2024-10-08 21:44:06 -0300

    Guarantees correct URL and HTTP method on get requests

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -25,6 +25,26 @@ class URLSessionHTTPCLient {
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
+	func test_getFromURL_performsGETRequestWithURL() {
+		URLProtocolStub.startInterceptingRequests()
+		
+		let url = URL(string: "http://any-url.com")!
+		let expectation = expectation(description: "Wait for request")
+		
+		URLProtocolStub.observeRequests { request in
+			XCTAssertEqual(request.url, url)
+			XCTAssertEqual(request.httpMethod, "GET")
+			
+			expectation.fulfill()
+		}
+		
+		URLSessionHTTPCLient().get(from: url) { _ in }
+		
+		wait(for: [expectation], timeout: 1)
+		
+		URLProtocolStub.stopInterceptingRequests()
+	}
+	
 	func test_getFromURL_failsOnRequestError() {
 		URLProtocolStub.startInterceptingRequests()
 		let url = URL(string: "http://any-url.com")!
@@ -56,6 +76,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
 	
 	private class URLProtocolStub: URLProtocol {
 		private static var stub: Stub?
+		private static var requestObserver: ((URLRequest) -> Void)?
 		
 		private struct Stub {
 			let data: Data?
@@ -67,6 +88,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
 			stub = Stub(data: data, response: response, error: error)
 		}
 		
+		static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+			requestObserver = observer
+		}
+		
 		static func startInterceptingRequests() {
 			URLProtocol.registerClass(URLProtocolStub.self)
 		}
@@ -74,9 +99,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
 		static func stopInterceptingRequests() {
 			URLProtocol.unregisterClass(URLProtocolStub.self)
 			stub = nil
+			requestObserver = nil
 		}
 		
 		override class func canInit(with request: URLRequest) -> Bool {
+			requestObserver?(request)
 			return true
 		}
 		
