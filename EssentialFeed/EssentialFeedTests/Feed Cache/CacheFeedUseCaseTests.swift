0b4a0fe6fb88bc8760288cd9c848418fe82063fb
Gustavo Guedes <gguedes.tech@gmail.com>
2024-10-14 22:25:53 -0300

    Extract "FeedCacheSpy" into a shared scope to remove duplication

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -142,44 +142,4 @@ class CacheFeedUseCaseTests: XCTestCase {
 	private func anyNSError() -> NSError {
 		return NSError(domain: "any error", code: 0)
 	}
-	
-	private class FeedStoreSpy: FeedStore {
-		var insertions = [(feed: [LocalFeedImage], timestamp: Date)]()
-		
-		enum ReceivedMessage: Equatable {
-			case deleteCachedFeed
-			case insert([LocalFeedImage], Date)
-		}
-		
-		private(set) var receivedMessagens = [ReceivedMessage]()
-		
-		private var deletionCompletions = [DeletionCompletion]()
-		private var insertionCompletions = [InsertionCompletion]()
-		
-		func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-			deletionCompletions.append(completion)
-			receivedMessagens.append(.deleteCachedFeed)
-		}
-		
-		func completeDeletion(with error: Error, at index: Int = 0) {
-			deletionCompletions[index](error)
-		}
-		
-		func completeDeletionSuccessfully(at index: Int = 0) {
-			deletionCompletions[index](nil)
-		}
-		
-		func completeInsertion(with error: Error, at index: Int = 0) {
-			insertionCompletions[index](error)
-		}
-		
-		func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-			insertionCompletions.append(completion)
-			receivedMessagens.append(.insert(feed, timestamp))
-		}
-		
-		func completeInsertionSuccessfully(at index: Int = 0) {
-			insertionCompletions[index](nil)
-		}
-	}
 }
--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -23,44 +23,4 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return (sut, store)
 	}
-	
-	private class FeedStoreSpy: FeedStore {
-		var insertions = [(feed: [LocalFeedImage], timestamp: Date)]()
-		
-		enum ReceivedMessage: Equatable {
-			case deleteCachedFeed
-			case insert([LocalFeedImage], Date)
-		}
-		
-		private(set) var receivedMessagens = [ReceivedMessage]()
-		
-		private var deletionCompletions = [DeletionCompletion]()
-		private var insertionCompletions = [InsertionCompletion]()
-		
-		func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-			deletionCompletions.append(completion)
-			receivedMessagens.append(.deleteCachedFeed)
-		}
-		
-		func completeDeletion(with error: Error, at index: Int = 0) {
-			deletionCompletions[index](error)
-		}
-		
-		func completeDeletionSuccessfully(at index: Int = 0) {
-			deletionCompletions[index](nil)
-		}
-		
-		func completeInsertion(with error: Error, at index: Int = 0) {
-			insertionCompletions[index](error)
-		}
-		
-		func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-			insertionCompletions.append(completion)
-			receivedMessagens.append(.insert(feed, timestamp))
-		}
-		
-		func completeInsertionSuccessfully(at index: Int = 0) {
-			insertionCompletions[index](nil)
-		}
-	}
 }
