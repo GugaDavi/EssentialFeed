d14f5023b39909e1fc1e798655ef198b379a8078
Gustavo Guedes <gguedes.tech@gmail.com>
2024-10-15 09:21:31 -0300

    Load command does not delete less than seven days old cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -91,6 +91,18 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		XCTAssertEqual(store.receivedMessagens, [.retrieve])
 	}
 	
+	func test_load_doesNotDeleteCacheOnLessThanSevenDaysOldCache() {
+		let feed = uniqueImageFeed()
+		let fixedCurrentDate = Date()
+		let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+		let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+		
+		sut.load { _ in }
+		store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+		
+		XCTAssertEqual(store.receivedMessagens, [.retrieve])
+	}
+	
 	//MARK: - Helpers
 	
 	private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
