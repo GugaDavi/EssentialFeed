00576153e19a6150f1cfbaeedbbcf85de21cc1a4
Gustavo Guedes <gguedes.tech@gmail.com>
2024-10-15 19:52:04 -0300

    Extract cache deletion side-effects on expired cahce from the "load" method to the "validateCache" method

--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -44,6 +44,30 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
 		
 		XCTAssertEqual(store.receivedMessagens, [.retrieve])
 	}
+
+	func test_validateCache_deletesSevenDaysOldCache() {
+		let feed = uniqueImageFeed()
+		let fixedCurrentDate = Date()
+		let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+		let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+		
+		sut.validateCache()
+		store.completeRetrieval(with: feed.local, timestamp: sevenDaysOldTimestamp)
+		
+		XCTAssertEqual(store.receivedMessagens, [.retrieve, .deleteCachedFeed])
+	}
+	
+	func test_validateCache_deletesMoreThanSevenDaysOldCache() {
+		let feed = uniqueImageFeed()
+		let fixedCurrentDate = Date()
+		let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+		let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+		
+		sut.validateCache()
+		store.completeRetrieval(with: feed.local, timestamp: moreThanSevenDaysOldTimestamp)
+		
+		XCTAssertEqual(store.receivedMessagens, [.retrieve, .deleteCachedFeed])
+	}
 	
 	//MARK: - Helpers
 	
