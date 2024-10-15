55920508689037321977f5944fae8e7a09d44821
Gustavo Guedes <gguedes.tech@gmail.com>
2024-10-14 23:33:04 -0300

    Load command delivers cached images on less than seven days old cache

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -7,13 +7,19 @@
 
 import Foundation
 
+public enum RetrievalcachedFeedResult {
+	case empty
+	case found(feed: [LocalFeedImage], timestamp: Date)
+	case failure(Error)
+}
+
 public protocol FeedStore {
 	typealias DeletionCompletion = (Error?) -> Void
 	typealias InsertionCompletion = (Error?) -> Void
-	typealias RetrievalCompletion = (Error?) -> Void
+	typealias RetrievalCompletion = (RetrievalcachedFeedResult) -> Void
 	
 	func deleteCachedFeed(completion: @escaping DeletionCompletion)
 	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
-	func retrieve(completion: @escaping InsertionCompletion)
+	func retrieve(completion: @escaping RetrievalCompletion)
 }
 
--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -40,6 +40,17 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		})
 	}
 	
+	func test_load_deliversCachedImagesOnLessThenSevenDaysOldCache() {
+		let feed = uniqueImageFeed()
+		let fixedCurrentDate = Date()
+		let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+		let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+		
+		expect(sut, toCompleteWith: .success(feed.models), when: {
+			store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+		})
+	}
+	
 	//MARK: - Helpers
 	
 	private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -72,5 +83,30 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 	
 	private func anyNSError() -> NSError {
 		return NSError(domain: "any error", code: 0)
+	}
+	
+	private func uniqueFeedImage() -> FeedImage {
+		return FeedImage(id: UUID(), url: anyURL())
+	}
+	
+	private func anyURL() -> URL {
+		return URL(string: "http://any-url.com")!
+	}
+	
+	private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+		let models = [uniqueFeedImage(), uniqueFeedImage()]
+		let local = models.map { LocalFeedImage(id: $0.id, url: $0.url, description: $0.description, location: $0.location) }
+		
+		return (models, local)
+	}
+}
+
+private extension Date {
+	func adding(days: Int) -> Date {
+		return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+	}
+	
+	func adding(seconds: TimeInterval) -> Date {
+		return self + seconds
 	}
 }
