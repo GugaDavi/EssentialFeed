a97411f945fe623142dd9ecbbb7a994f22497fe6
Gustavo Guedes <gguedes.tech@gmail.com>
2024-10-17 09:35:14 -0300

    Break down "CodableFeedStore" insertion tests methods guarantee there's only one assertion per test. The goal is to clarify the behavior under test in small units, so we can extract the behavior tests into reusable specs.

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,6 +8,36 @@
 import XCTest
 import EssentialFeed
 
+protocol FeedStoreSpecs {
+	func test_retrieve_deliversEmptyData()
+	func test_retrieve_hasNoSideEffectsOnEmptyCache()
+	func test_retrieve_deliversFoundValuesOnNonEmptyCache()
+	func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
+	
+	func test_insert_deliversNoErrorOnEmptyCache()
+	func test_insert_deliversNoErrorOnNonEmptyCache()
+	func test_insert_overridesPreviouslyInsertedCacheValues()
+	
+	func test_delete_hasNoSideEffectsOnEmptyCache()
+	func test_delete_emptiesPrevioslyInsertedCache()
+	
+	func test_storeSideEffects_runSerially()
+}
+
+protocol FailableRetrieveFeedStoreSpecs {
+	func test_retrieve_deliversFailureOnRetrievalError()
+	func test_retrieve_hasNoSideEffectsOnFailure()
+}
+
+protocol FailableInsertFeedStoreSpecs {
+	func test_insert_deliversErrorOnInsertionError()
+	func test_insert_hasNoSideEffectsOnInsertionError()
+}
+
+protocol FailableDeleteFeedStoreSpecs {
+	func test_delete_deliversErrorOnDeletionError()
+}
+
 final class CodableFeedStoreTests: XCTestCase {
 	override func setUp() {
 		super.setUp()
@@ -71,16 +101,29 @@ final class CodableFeedStoreTests: XCTestCase {
 		expect(sut, toRetrieveTwice: .failure(anyNSError()))
 	}
 	
+	func test_insert_deliversNoErrorOnEmptyCache() {
+		let sut = makeSUT()
+		
+		let insertionError = insert((uniqueImageFeed().local, Date()), to: sut)
+		
+		XCTAssertNil(insertionError)
+	}
+	
+	func test_insert_deliversNoErrorOnNonEmptyCache() {
+		let sut = makeSUT()
+		insert((uniqueImageFeed().local, Date()), to: sut)
+		
+		let insertionError = insert((uniqueImageFeed().local, Date()), to: sut)
+		
+		XCTAssertNil(insertionError)
+	}
+	
 	func test_insert_overridesPreviouslyInsertedCacheValues() {
 		let sut = makeSUT()
 		
-		let firstInsertionError = insert((uniqueImageFeed().local, Date()), to: sut)
-		XCTAssertNil(firstInsertionError)
-		
 		let latestFeed = uniqueImageFeed().local
 		let latestTimestamp = Date()
-		let latestInserionError = insert((latestFeed, latestTimestamp), to: sut)
-		XCTAssertNil(latestInserionError)
+		insert((latestFeed, latestTimestamp), to: sut)
 		
 		expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
 	}
@@ -94,6 +137,16 @@ final class CodableFeedStoreTests: XCTestCase {
 		let insertionError = insert((feed, timestamp), to: sut)
 		
 		XCTAssertNotNil(insertionError)
+	}
+	
+	func test_insert_hasNoSideEffectsOnInsertionError() {
+		let invalidStoreURL = URL(string: "invalid-store-url")
+		let sut = makeSUT(storeURL: invalidStoreURL)
+		let feed = uniqueImageFeed().local
+		let timestamp = Date()
+		
+		insert((feed, timestamp), to: sut)
+		
 		expect(sut, toRetrieve: .empty)
 	}
 	
