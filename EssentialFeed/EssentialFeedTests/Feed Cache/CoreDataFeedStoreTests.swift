38fc272a31cc32a56891b4fce2f34b17ee2ec39b
Gustavo Guedes <gguedes.tech@gmail.com>
2024-10-17 12:52:07 -0300

    Add empty CoreData feed store specs

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,59 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Gustavo Guedes on 17/10/24.
+//
+
+import XCTest
+
+final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+	func test_retrieve_deliversEmptyData() {
+		<#code#>
+	}
+	
+	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+		<#code#>
+	}
+	
+	func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+		<#code#>
+	}
+	
+	func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+		<#code#>
+	}
+	
+	func test_insert_deliversNoErrorOnEmptyCache() {
+		<#code#>
+	}
+	
+	func test_insert_deliversNoErrorOnNonEmptyCache() {
+		<#code#>
+	}
+	
+	func test_insert_overridesPreviouslyInsertedCacheValues() {
+		<#code#>
+	}
+	
+	func test_delete_deliversNoErrorOnEmptyCache() {
+		<#code#>
+	}
+	
+	func test_delete_hasNoSideEffectsOnEmptyCache() {
+		<#code#>
+	}
+	
+	func test_delete_deliversNoErrorOnNonEmptyCache() {
+		<#code#>
+	}
+	
+	func test_delete_emptiesPreviouslyInsertedCache() {
+		<#code#>
+	}
+	
+	func test_storeSideEffects_runSerially() {
+		<#code#>
+	}
+	
+}
